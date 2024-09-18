a0c46fc9fc51e379595147790cc7fe01c9390938
anurag <anuragmishra4499@gmail.com>
2024-09-18 21:13:54 +0530

    new commit

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,4 +1,4 @@
-const cloudinary = require('cloudinary').v2;
+import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
   cloud_name: dx9xzfhtp,
@@ -6,4 +6,4 @@ cloudinary.config({
   api_secret: zhYXijzQSo0mSh155s3Iri_tJTs
 });
 
-module.exports = cloudinary;
+export default cloudinary;
